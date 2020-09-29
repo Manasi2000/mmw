b382efa512d6e89056e9de22dd3b03a41f892cbe
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-09-29 13:39:26 +0530

    js added

---
+++ js/javasc.js
@@ -0,0 +1,14 @@
+$('body').scrollspy({ target: '#navbar-example' })
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    
+$('.first-button').on('click', function () {
+
+$('.animated-icon1').toggleClass('open');
+
+$('.first-button').on('click', function () {
+
+$('.animated-icon1').toggleClass('close');
+});
+});
+  });
